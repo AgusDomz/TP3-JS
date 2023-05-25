70afa06da6fee352c0ee37ea8e71b693a7830a65
agusdomz <agusdominguez95@gmail.com>
2023-05-25 20:09:25 -0300

    archivo base

---
+++ app.js
@@ -0,0 +1,90 @@
+"use strict"; //sirve para tener que definir si o si las variables
+/*
+console.log(source);
+
+print();
+
+function print() {
+    console.log("desde print: " + source);
+}
+
+var source = "AgusDomz"
+
+print();
+
+
+let ironman = {
+    nombre : "Tony Stark",
+    alias : "ironman",
+    edad : 54,
+    mecanico : "pipero",
+    grupo : "vengadores",
+}
+
+let propiedades = ["nombre", "alias", "edad", "mecanico", "grupo"];
+
+for (let i = 0; i < Object.keys(ironman).length; i++) {
+    console.log(ironman[propiedades[i]]);
+}
+*/
+
+/*
+1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    console.log("Los meses del año son: ");
+    meses.forEach(function (mes) {
+        console.log(mes);
+    });
+
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+    -Mostrar la longitud del arreglo.
+    -Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+    -Añade en última posición la ciudad de París.
+    -Escribe por pantalla el elemento que ocupa la segunda posición.
+    -Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+
+    -La fórmula del perímetro  es p = 2*(a +b)
+
+*/
+//-----------------------------------------------------------------------------------
+/*
+
+7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+
+*/
+
+
+
